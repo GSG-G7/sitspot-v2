b1c09ef20c3a35cf83c7cffd04b6154c6d4f3d69
amoodaa <amoodaa@gmail.com>
2019-10-27 15:06:05 +0200

    Floating action button refined relates #18

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Fab } from '../components';
 import './style.css';
 
 const App = () => {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Fab onClick={() => {}} />
+    </div>
+  );
 };
 
 export default App;
--- client/src/components/Fab/index.js
+++ client/src/components/Fab/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import propTypes from 'prop-types';
-import { Icon } from 'antd';
 import './style.css';
 
 const Fab = ({ onClick }) => (
-  <button type="button" onClick={onClick} className="floating-action">
-    <Icon type="plus" />
+  <button
+    type="button"
+    onClick={onClick}
+    className="circle-main-color-button floating plus-text"
+  >
+    +
   </button>
 );
+
 Fab.propTypes = {
   onClick: propTypes.func.isRequired,
 };
