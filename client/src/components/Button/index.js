b3c4e02555685513f6e9ffc12df2f89f06278f0c
amoodaa <amoodaa@gmail.com>
2019-10-28 09:35:35 +0200

    removed style props

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -2,24 +2,17 @@ import React from 'react';
 import propTypes from 'prop-types';
 import './style.css';
 
-const Button = ({ onClick, className, children, style }) => (
-  <button
-    type="button"
-    onClick={onClick}
-    className={`button ${className}`}
-    style={style}
-  >
+const Button = ({ onClick, className, children }) => (
+  <button type="button" onClick={onClick} className={`button ${className}`}>
     {children}
   </button>
 );
 Button.defaultProps = {
-  style: {},
   children: [],
 };
 Button.propTypes = {
   onClick: propTypes.func.isRequired,
   className: propTypes.string.isRequired,
-  style: propTypes.shape(),
   children: propTypes.arrayOf(propTypes.element),
 };
 
