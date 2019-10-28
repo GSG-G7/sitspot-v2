84b77b81d7ba476a481d5a92707bfc64fa2a59f6
asem1789 <as.1789@hotmail.com>
2019-10-28 13:55:25 +0200

    replace BrwserRouter in index file Relates #20

--- client/src/components/Menu/index.js
+++ client/src/components/Menu/index.js
@@ -1,37 +1,36 @@
 import React from 'react';
-import { NavLink, BrowserRouter } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { Icon } from 'antd';
+
 import './index.css';
 
 const Menu = () => {
   return (
-    <BrowserRouter>
-      <div className="menu">
-        <ul className="menu__list">
-          <li>
-            <NavLink className="menu__list__item" to="/review">
-              Add your recommendation
-            </NavLink>
-            <Icon className="menu__list__close" type="close" />
-          </li>
-          <li>
-            <NavLink className="menu__list__item" to="/faq">
-              FAQ
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className="menu__list__item" to="/about-us">
-              About us
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className="menu__list__item" to="/contact">
-              Contact
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-    </BrowserRouter>
+    <div className="menu">
+      <ul className="menu__list">
+        <li>
+          <NavLink className="menu__list__item" to="/review">
+            Add your recommendation
+          </NavLink>
+          <Icon className="menu__list__close" type="close" />
+        </li>
+        <li>
+          <NavLink className="menu__list__item" to="/faq">
+            FAQ
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="menu__list__item" to="/about-us">
+            About us
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="menu__list__item" to="/contact">
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+    </div>
   );
 };
 
