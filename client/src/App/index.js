6c032871741476801b6f94cf5a30159d4ecbc3cf
amoodaa <amoodaa@gmail.com>
2019-10-29 00:26:50 +0200

    how to use this component, remember to delete!!!

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
-
+import { MagicalFactor } from '../components';
 import './style.css';
+import Keywords from '../components/MagicalFactor/keywords';
 
 const App = () => {
-  return <div className="App"> </div>;
+  const { family, solo, wildlife } = Keywords;
+  return (
+    <div className="App">
+      <MagicalFactor
+        value={family}
+        reviewText="HI its was soooooooo  nieeeeuuaskldjas"
+      />
+      <MagicalFactor
+        value={wildlife}
+        reviewText="HI its was soooooooo asddddddddddddddddddddddddddddddddnieeeeuuaskldjasasddddddddddddddddddddddddddddddddnieeeeuuaskldjasasddddddddddddddddddddddddddddddddnieeeeuuaskldjasasddddddddddddddddddddddddddddddddnieeeeuuaskldjas asddddddddddddddddddddddddddddddddnieeeeuuaskldjas"
+      />
+      <MagicalFactor
+        value={solo}
+        reviewText="HI its was soooooooo  nieeeeuuaskldjas"
+      />
+    </div>
+  );
 };
 
 export default App;
