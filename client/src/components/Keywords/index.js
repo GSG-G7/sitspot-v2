9ed818d259de3ddb13328283b9dba0a1a4464b6c
asem1789 <as.1789@hotmail.com>
2019-10-28 09:42:58 +0200

    editing style Relates #17

--- client/src/components/Keywords/index.js
+++ client/src/components/Keywords/index.js
@@ -12,8 +12,8 @@ class Keywords extends Component {
   handleKeywordChange = keywords => {
     this.setState({ value: keywords.value });
     // this function will passed in this component to take a value
-    const { getKeyword } = this.props;
-    getKeyword(keywords.value);
+    // const { getKeyword } = this.props;
+    // getKeyword(keywords.value);
   };
 
   render() {
@@ -23,48 +23,23 @@ class Keywords extends Component {
         <Icon className="keywords__close" type="close" />
         <h2 className="keywords__title">Keywords</h2>
         <div className="keywords__list">
-          <div className="keywords__list__left">
-            {keywordsData
-              .slice(0, keywordsData.length / 2 + 1)
-              .map((el, index) => {
-                return (
-                  <button
-                    key={`keyword${index + 1}`}
-                    className={
-                      value && el.value === value
-                        ? 'keywords__list--item active-btn'
-                        : 'keywords__list--item'
-                    }
-                    value={el.value}
-                    type="button"
-                    onClick={btn => this.handleKeywordChange(btn.target)}
-                  >
-                    {el.text}
-                  </button>
-                );
-              })}
-          </div>
-          <div className="keywords__list__right">
-            {keywordsData
-              .slice(keywordsData.length / 2 + 1, keywordsData.length)
-              .map((el, index) => {
-                return (
-                  <button
-                    key={`keyword${index + 1}`}
-                    className={
-                      value && el.value === value
-                        ? 'keywords__list--item active-btn'
-                        : 'keywords__list--item'
-                    }
-                    value={el.value}
-                    type="button"
-                    onClick={btn => this.handleKeywordChange(btn.target)}
-                  >
-                    {el.text}
-                  </button>
-                );
-              })}
-          </div>
+          {keywordsData.map((el, index) => {
+            return (
+              <button
+                key={`keyword${index + 1}`}
+                className={
+                  value && el.value === value
+                    ? 'keywords__list--item active-btn'
+                    : 'keywords__list--item'
+                }
+                value={el.value}
+                type="button"
+                onClick={btn => this.handleKeywordChange(btn.target)}
+              >
+                {el.text}
+              </button>
+            );
+          })}
         </div>
       </div>
     );
