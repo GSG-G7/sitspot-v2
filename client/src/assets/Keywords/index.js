755f6f27bc10a0227177b2df8071dc4557e49f39
amoodaa <amoodaa@gmail.com>
2019-10-29 00:27:16 +0200

    modulary imports data collections

---
+++ client/src/assets/Keywords/index.js
@@ -0,0 +1,33 @@
+import family from './family.png';
+import solo from './solo.png';
+import wild from './wild.png';
+// import commitment from './commitment.png';
+// import interaction from './interaction.png';
+// import kid from './kid.png';
+// import immersion from './immersion.png';
+// import luxury from './luxury.png';
+// import nature from './nature.png';
+// import rural from './rural.png';
+// import social from './social.png';
+// import staff from './staff.png';
+// import urban from './urban.png';
+// import vegan from './vegan.png';
+// import vegetarian from './vegetarian.png';
+
+export default {
+  solo,
+  wild,
+  family,
+  //   commitment,
+  //   interaction,
+  //   kid,
+  //   immersion,
+  //   luxury,
+  //   nature,
+  //   rural,
+  //   social,
+  //   staff,
+  //   urban,
+  //   vegan,
+  //   vegetarian,
+};
---
+++ client/src/components/MagicalFactor/keywords.js
@@ -0,0 +1,22 @@
+const keywordsData = {
+  solo: { text: 'Solo traveller friendly', src: 'solo' },
+  wildlife: { text: 'Great for wildlife', src: 'wild' },
+  family: { text: 'Family owned', src: 'family' },
+  // vegetarian: { text: 'Vegetarian friendly', src: 'vegetarian.png' },
+  // wellbeing: { text: 'Great for wellbeing', src: 'wellbeing.png' },
+  // vegan: { text: 'Vegan', src: 'vegan.png' },
+  // makingFriends: {
+  //   text: 'Great for making new friends',
+  //   src: 'social.png',
+  // },
+  // budget: { text: 'Budget', src: '.png' },
+  // nature: { text: 'Great for nature', src: 'nature.png' },
+  // kids: { text: 'Kids friendly', src: 'kid.png' },
+  // culture: { text: 'Great for local culture', src: '.png' },
+  // midRange: { text: 'Mid range', src: '.png' },
+  // staff: { text: 'Great staff', src: 'staff.png' },
+  // luxury: { text: 'Luxury', src: 'luxury.png' },
+  // owners: { text: 'Great owners/hosts', src: '.png' },
+};
+
+export default keywordsData;
