43f6b497435214ab03de8dd5609d64c36e9043d3
asem1789 <as.1789@hotmail.com>
2019-10-29 14:16:12 +0200

    make footer component Relates #48

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,5 +2,6 @@ import Keywords from './Keywords';
 import Button from './Button';
 import Menu from './Menu';
 import Fab from './Fab';
+import Footer from './Footer';
 
-export { Button, Menu, Keywords, Fab };
+export { Button, Menu, Keywords, Fab, Footer };
