cb20e4a47088f2848f2a6db145d87ed3154d93c5
mossa-Sammer <mossa708496@gmail.com>
2019-10-29 10:53:08 +0200

    export the component in indexjs file relates #22

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,5 +2,6 @@ import Keywords from './Keywords';
 import Button from './Button';
 import Menu from './Menu';
 import Fab from './Fab';
+import Search from './Search';
 
-export { Button, Menu, Keywords, Fab };
+export { Button, Menu, Keywords, Fab, Search };
