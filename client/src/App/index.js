03a6c450588ba64618363cc3ff67bd0234ae1cfe
amoodaa <amoodaa@gmail.com>
2019-10-29 10:33:58 +0200

    cleared app

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import { MagicalFactor } from '../components';
+
 import './style.css';
-import Keywords from '../staticDataSet/keywords';
 
 const App = () => {
-  const { family, solo, wildlife } = Keywords;
-  return (
-    <div className="App">
-      <MagicalFactor
-        value={family}
-        reviewText="HI its was soooooooo  nieeeeuuaskldjas"
-      />
-      <MagicalFactor
-        value={wildlife}
-        reviewText="HI its was soooooooo asddddddddddddddddddddddddddddddddnieeeeuuaskldjasasddddddddddddddddddddddddddddddddnieeeeuuaskldjasasddddddddddddddddddddddddddddddddnieeeeuuaskldjasasddddddddddddddddddddddddddddddddnieeeeuuaskldjas asddddddddddddddddddddddddddddddddnieeeeuuaskldjas"
-      />
-      <MagicalFactor
-        value={solo}
-        reviewText="HI its was soooooooo  nieeeeuuaskldjas"
-      />
-    </div>
-  );
+  return <div className="App"> </div>;
 };
 
 export default App;
