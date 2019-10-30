6b68d931a7d07c2e3ce5cbe84494532826718cb0
asem1789 <as.1789@hotmail.com>
2019-10-30 13:14:46 +0200

    separate question content in Collapse component Relates 27#

--- client/src/pages/FAQ/index.js
+++ client/src/pages/FAQ/index.js
@@ -1,70 +1,10 @@
 import React from 'react';
-import { Collapse } from 'antd';
 import questions from './fakeData';
+import { CollapsePanel } from '../../components';
 
 import './index.css';
 
 const FAQ = () => {
-  const { Panel } = Collapse;
-
-  const handlerPanel = arr => {
-    const mainQuestions = [];
-    arr.forEach((mainQuestion, index) => {
-      if (mainQuestion.contentSubQ) {
-        mainQuestions.push(
-          <Panel key={`key${index + 1}`} header={mainQuestion.title}>
-            <Collapse defaultActiveKey="key1">
-              {mainQuestion.contentSubQ.map((subQuestion, index1) => {
-                return (
-                  <Panel
-                    className="sub-title"
-                    header={subQuestion.title}
-                    key={`key${index1 + 1}`}
-                  >
-                    {subQuestion.content.map((questionContent, index2) => {
-                      const TagName = questionContent.tag;
-
-                      return (
-                        <TagName key={`key${index2 + 1}`}>
-                          {questionContent.value}
-                        </TagName>
-                      );
-                    })}
-                  </Panel>
-                );
-              })}
-            </Collapse>
-          </Panel>
-        );
-      } else {
-        mainQuestions.push(
-          <Panel key={`key${index + 1}`} header={mainQuestion.title}>
-            {mainQuestion.content.map((questionContent, index3) => {
-              const TagName = questionContent.tag;
-              if (questionContent.parent) {
-                const ParentTag = questionContent.parent;
-                return (
-                  <ParentTag key={`key${index3 + 1}`}>
-                    {questionContent.value.map((value, index4) => (
-                      <TagName key={`key${index4 + 1}`}>{value}</TagName>
-                    ))}
-                  </ParentTag>
-                );
-              }
-              return (
-                <TagName key={`key${index3 + 1}`}>
-                  {questionContent.value}
-                </TagName>
-              );
-            })}
-          </Panel>
-        );
-      }
-    });
-
-    return mainQuestions;
-  };
-
   return (
     <div className="faq">
       <div className="faq__header">
@@ -80,9 +20,7 @@ const FAQ = () => {
           </span>{' '}
           for a chat.
         </p>
-        <div id="faq1" className="faq__content__questions">
-          <Collapse accordion>{handlerPanel(questions)}</Collapse>
-        </div>
+        <CollapsePanel questions={questions} />
       </div>
     </div>
   );
