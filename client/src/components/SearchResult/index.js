ea4a291f91d6b394b88cbd4b5f1b2de36f556bd3
asem1789 <as.1789@hotmail.com>
2019-10-31 15:45:25 +0200

    make destructuring and edit propTypes Relates #16

--- client/src/components/SearchResult/index.js
+++ client/src/components/SearchResult/index.js
@@ -7,14 +7,14 @@ import './style.css';
 const SearchResult = ({ resultArray }) => (
   <div className="search-result">
     <ul className="search-result-list">
-      {resultArray.map(item => (
-        <li key={item.id} className="search-result__item">
+      {resultArray.map(({ id, type, address }) => (
+        <li key={id} className="search-result__item">
           <img
             className="search-result__item-img"
             alt="one sitSpot"
-            src={ImageRepository[item.type]}
+            src={ImageRepository[type]}
           />
-          <p className="search-result__item-text">{item.address}</p>
+          <p className="search-result__item-text">{address}</p>
         </li>
       ))}
     </ul>
@@ -22,7 +22,13 @@ const SearchResult = ({ resultArray }) => (
 );
 
 SearchResult.propTypes = {
-  resultArray: propTypes.arrayOf(propTypes.object).isRequired,
+  resultArray: propTypes.arrayOf(
+    propTypes.shape({
+      id: propTypes.number,
+      type: propTypes.string,
+      address: propTypes.string,
+    })
+  ).isRequired,
 };
 
 export default SearchResult;
