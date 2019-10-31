8af838e33d6141f7d4059e99a998cb921d724162
mossa-Sammer <mossa708496@gmail.com>
2019-10-31 14:50:59 +0200

    added fake data relates #26

---
+++ client/src/components/ImageCarousel/fakeData.js
@@ -0,0 +1,15 @@
+const fakeImages = [
+  {
+    src:
+      'https://upload.wikimedia.org/wikipedia/commons/6/62/Paracas_National_Reserve%2C_Ica%2C_Peru-3April2011.jpg',
+    alt: 'good Image',
+  },
+  {
+    src:
+      'https://upload.wikimedia.org/wikipedia/commons/6/62/Paracas_National_Reserve%2C_Ica%2C_Peru-3April2011.jpg',
+    alt: 'ddd ',
+    title: 'Cool Image',
+  },
+];
+
+export default fakeImages;
