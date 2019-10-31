1218fafcaa588290dd80ba1e39bd956f4b67b5ce
amoodaa <amoodaa@gmail.com>
2019-10-31 11:00:53 +0200

    merged from master

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -5,5 +5,15 @@ import Fab from './Fab';
 import Header from './Header';
 import Footer from './Footer';
 import MagicalFactor from './MagicalFactor';
+import Review from './Review';
 
-export { Button, Menu, KeywordList, Fab, Header, Footer, MagicalFactor };
+export {
+  Button,
+  Menu,
+  KeywordList,
+  Fab,
+  Header,
+  Footer,
+  MagicalFactor,
+  Review,
+};
