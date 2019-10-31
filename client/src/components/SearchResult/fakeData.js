b6a40a45385ed7e4c258b23f664d194ce92af112
asem1789 <as.1789@hotmail.com>
2019-10-31 09:36:29 +0200

    formatting style  and make fakeData file Relates #16

---
+++ client/src/components/SearchResult/fakeData.js
@@ -0,0 +1,11 @@
+const searchData = [
+  { id: 1, address: 'W6 Garden Cafe, London', imgSrc: 'hotel' },
+  { id: 2, address: 'St Daniel hotel, Solvenia', imgSrc: 'restaurant' },
+  { id: 3, address: 'Hetu Bulk shop, London', imgSrc: 'hotel' },
+  { id: 4, address: 'W6 Garden Cafe, London', imgSrc: 'hotel' },
+  { id: 5, address: 'St Daniel hotel, Solvenia', imgSrc: 'restaurant' },
+  { id: 6, address: 'Hetu Bulk shop, London', imgSrc: 'hotel' },
+  { id: 7, address: 'St Daniel hotel, Solvenia', imgSrc: 'restaurant' },
+];
+
+export default searchData;
--- client/src/components/SearchResult/index.js
+++ client/src/components/SearchResult/index.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
-import image from '../../assets/images/placeimg.png';
+import sitspotImg from '../../assets/searchResult';
 import './style.css';
 
-const SearchResult = ({ resultArray }) => (
-  <ul className="list">
-    {resultArray.map(item => {
-      return (
-        <li key={item.id} className="list__item">
-          <img className="list__item__img" alt="one sitSpot" src={image} />
-          <p className="list__item__text">Consect Adip</p>
-          <p className="list__item__text">Etiam Dapibus</p>
-        </li>
-      );
-    })}
-  </ul>
-);
+const SearchResult = ({ resultArray }) => {
+  let counter = 1;
+  return (
+    <ul className="list">
+      {resultArray.map(item => {
+        // this class to avoid margin in the third img in every row
+        let classNames = '';
+        if (counter === 3) {
+          counter = 1;
+          classNames = 'list__item list__item__last__row';
+        } else {
+          classNames = 'list__item';
+          counter += 1;
+        }
+
+        return (
+          <li key={item.id} className={classNames}>
+            <img
+              className="list__item__img"
+              alt="one sitSpot"
+              src={sitspotImg[item.imgSrc]}
+            />
+            <p className="list__item__text">{item.address}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 SearchResult.propTypes = {
   resultArray: propTypes.arrayOf(propTypes.object).isRequired,
