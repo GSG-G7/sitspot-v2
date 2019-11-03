e07e6d1b4b56f1a873abf79fbc1a89625cc8c464
amoodaa <amoodaa@gmail.com>
2019-11-03 10:43:48 +0200

    added a sub component for profile details

--- client/src/components/Review/subcomponent/ProfileDetails.js
+++ client/src/components/Review/subcomponent/ProfileDetails.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
-const ProfileDetails = ({ profile }) =>
-  Object.entries(profile).map(([key, value]) => (
-    <div key={key}>
-      {key}: {value}
-    </div>
-  ));
-
+const ProfileDetails = ({ profile }) => (
+  <div className="profile-details">
+    {Object.entries(profile).map(([key, value]) => (
+      <div key={key} className="profile-details__item">
+        <div className="profile-details__key">{key}</div>
+        <div className="profile-details__value">{value}</div>
+      </div>
+    ))}
+  </div>
+);
 ProfileDetails.propTypes = {
-  reviewee: propTypes.shape({
-    name: propTypes.string.isRequired,
+  profile: propTypes.shape({
+    Name: propTypes.string.isRequired,
+    Country: propTypes.string.isRequired,
+    Age: propTypes.string.isRequired,
   }).isRequired,
 };
 export default ProfileDetails;
