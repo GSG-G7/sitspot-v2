10688d4e4651f66f5b61002c13ccc9bc39848b7e
asem1789 <as.1789@hotmail.com>
2019-11-04 13:24:24 +0200

    change name of props: style to classes  in aboutUs and  FAQ page Relates #51 #27

--- client/src/pages/aboutUs/index.js
+++ client/src/pages/aboutUs/index.js
@@ -4,6 +4,15 @@ import { CollapsePanel } from '../../components';
 
 import './index.css';
 
+const Classes = Object.freeze({
+  mainTitle: 'about__main-title',
+  pClass: 'about__paragraph',
+  ulClass: 'about--ul',
+  liClass: 'about--li',
+  imgClass: 'about--img',
+  spanClass: 'about__contact--email',
+});
+
 const AboutUs = () => {
   return (
     <div className="about">
@@ -11,17 +20,7 @@ const AboutUs = () => {
         <h3 className="about__header__title">About us</h3>
       </div>
       <div id="faq__questions" className="about__content">
-        <CollapsePanel
-          questions={questions}
-          style={{
-            mainTitle: 'about__main-title',
-            pClass: 'about__paragraph',
-            ulClass: 'about--ul',
-            liClass: 'about--li',
-            imgClass: 'about--img',
-            spanClass: 'about__contact--email',
-          }}
-        />
+        <CollapsePanel questions={questions} classes={Classes} />
       </div>
     </div>
   );
