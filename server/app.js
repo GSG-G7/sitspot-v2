60e17ebf080055ac6ff64980103e9a6b3c1ad7ba
amoodaa <amoodaa@gmail.com>
2019-11-05 10:24:48 +0200

    intiial backend file structure and initial test relates #11

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const { join } = require('path');
+const express = require('express');
+const router = require('./routes');
+
+const app = express();
+
+app.set('port', process.env.PORT || 5000);
+
+app.use('/api/v1', router);
+
+app.use(express.static(join(__dirname, '..', 'client', 'build')));
+
+app.all('*', (req, res) => {
+  res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'));
+});
+
+module.exports = app;
