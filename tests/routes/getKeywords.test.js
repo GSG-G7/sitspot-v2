0afdb19e222cfb82f77cd396dcd5b87cae6ee158
mossa-Sammer <mossa708496@gmail.com>
2019-11-06 20:39:54 +0200

    make routes test folder relates #46

---
+++ tests/routes/getKeywords.test.js
@@ -0,0 +1,20 @@
+const tape = require('tape');
+const supertest = require('supertest');
+const app = require('../../server/app');
+
+tape('get Keywords route', t => {
+  supertest(app)
+    .get('/api/v1/keywords')
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      if (err) {
+        t.error(err);
+        t.end();
+      } else {
+        const { data } = res.body;
+        t.equal(Array.isArray(data), true, 'data should be an array');
+        t.end();
+      }
+    });
+});
