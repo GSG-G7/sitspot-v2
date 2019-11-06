dbaffd9b14f41e12dca5bf299498dd96921af531
SajedaIsmail <eng.sajedaismail@gmail.com>
2019-11-06 16:29:46 +0200

    single place page is still in progress

--- client/src/pages/singlePlace/index.js
+++ client/src/pages/singlePlace/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import propTypes from 'prop-types';
 
 import { Button, Fab, ImageCarousel } from '../../components/index';
@@ -7,26 +7,36 @@ import backIcon from '../../assets/images/left-arrow.svg';
 
 import './style.css';
 
-const SinglePlace = ({ name, country, city, website, images, reviews }) => (
-  <>
-    <div className="Header-div" />
-    <div className="buttons-container">
-      <span className="buttons-container__back">
-        <img syle="flaot: left" alt="back arrow" src={backIcon} />
-        <small>Back to results</small>
-      </span>
-      <Button onClick={() => {}} className="buttons-container__add">
-        Add your recommendation
-      </Button>
-    </div>
-    <span className="heading-span">{`${name}, ${country}, ${city}`}</span>
-    <span>{website}</span>
-    <ImageCarousel slides={images} />
-    <span>Recommended by {reviews.length}</span>
-    <Fab />
-    <hr />
-  </>
-);
+class SinglePlace extends Component {
+  state = {
+    reviews: [],
+  };
+
+  render() {
+    const { name, country, city, website, images } = this.props;
+    const { reviews } = this.state;
+    return (
+      <>
+        <div className="Header-div" />
+        <div className="buttons-container">
+          <span className="buttons-container__back">
+            <img syle="flaot: left" alt="back arrow" src={backIcon} />
+            <small>Back to results</small>
+          </span>
+          <Button onClick={() => {}} className="buttons-container__add">
+            Add your recommendation
+          </Button>
+        </div>
+        <span className="heading-span">{`${name}, ${country}, ${city}`}</span>
+        <span>{website}</span>
+        <ImageCarousel slides={images} />
+        <span>Recommended by {reviews.length}</span>
+        <Fab />
+        <hr />
+      </>
+    );
+  }
+}
 
 SinglePlace.propTypes = {
   name: propTypes.string.isRequired,
@@ -34,7 +44,6 @@ SinglePlace.propTypes = {
   city: propTypes.string.isRequired,
   website: propTypes.string.isRequired,
   images: propTypes.arrayOf(String).isRequired,
-  reviews: propTypes.arrayOf(Object).isRequired,
 };
 
 export default SinglePlace;
