0124aa5a636a9872a8c4bf6cfaded1f95a6e0c1a
mossa-Sammer <mossa708496@gmail.com>
2019-11-06 20:41:29 +0200

    refactor the query relates #46

--- server/models/queries/getKeywords.js
+++ server/models/queries/getKeywords.js
@@ -1,13 +1,6 @@
 const base = require('../config');
 
-module.exports = () => {
-  let allRecords = [];
-  const process = (records, fetchNextPage) => {
-    allRecords = [...allRecords, ...records.map(record => record.get('Name'))];
-    fetchNextPage();
-  };
-  return base('Keywords')
+module.exports = () =>
+  base('Keywords')
     .select({ view: 'Grid view' })
-    .eachPage(process)
-    .then(() => allRecords);
-};
+    .firstPage();
