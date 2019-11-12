7266681c43eb8a034c9ab13934a72d4bd9bdb9c1
amoodaa <amoodaa@gmail.com>
2019-11-12 18:52:15 +0200

    bugfixing, added keywords support

--- server/models/queries/search.js
+++ server/models/queries/search.js
@@ -1,19 +1,24 @@
 const { typeformBase } = require('../config');
 
-const formulaMaker = ({ country, city, type }) => {
+const formulaMaker = ({ country, city, type, keywords }) => {
   const formula = [];
   if (country) formula.push(`IF({country} = '${country}',TRUE(),FALSE())`);
   if (city) formula.push(`IF({city} = '${city}',TRUE(),FALSE())`);
   if (type) formula.push(`IF({type} = '${type}',TRUE(),FALSE())`);
-  // Not possible to do keywords
+  keywords.forEach(e => formula.push(`{${e}}`));
   formula.push('{active}');
   return `AND(${formula.join(',')})`;
 };
 
-module.exports = ({ country = null, city = null, type = null }) =>
-  typeformBase('places')
+module.exports = ({
+  country = null,
+  city = null,
+  type = null,
+  keywords = ['mf_management_interaction'],
+}) =>
+  typeformBase('place')
     .select({
       view: 'Grid view',
-      filterByFormula: formulaMaker({ country, city, type }),
+      filterByFormula: formulaMaker({ country, city, type, keywords }),
     })
     .firstPage();
