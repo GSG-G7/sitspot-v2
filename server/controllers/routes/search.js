6a434d1a2df8df77aa82ffa63ee35515d51abed9
amoodaa <amoodaa@gmail.com>
2019-11-13 00:59:24 +0200

    had to comment keywords, search on keywords doesn't work

--- server/controllers/routes/search.js
+++ server/controllers/routes/search.js
@@ -1,7 +1,17 @@
 const { search } = require('../../models/queries');
 
+const imgUrl = publicId =>
+  `https://res.cloudinary.com/amoodaa/image/upload/v1573596346/${publicId}.png`;
+
 module.exports = (req, res, next) =>
   search(req.query)
-    .then(result => result.map(({ id, fields }) => ({ id, ...fields })))
+    .then(result =>
+      result.map(({ id, fields: { image1, image2, ...fields } }) => ({
+        id,
+        image1: imgUrl(image1),
+        image2: imgUrl(image2),
+        ...fields,
+      }))
+    )
     .then(result => res.json(result))
     .catch(next);
