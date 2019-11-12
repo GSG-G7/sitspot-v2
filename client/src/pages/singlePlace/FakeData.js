98eee2df749502d2063660056b706f3bb8fbfcd1
SajedaIsmail <eng.sajedaismail@gmail.com>
2019-11-12 16:12:54 +0200

    Refactor Relates #34

--- client/src/pages/singlePlace/FakeData.js
+++ client/src/pages/singlePlace/FakeData.js
@@ -1,8 +1,8 @@
-const singleSitSpot={
-  name: "Dkhhkj jlkhl", 
-  country: "UK", 
-  city: "London", 
-  website: "http://hkjhk.com",
-}
+const singleSitSpot = {
+  name: 'Dkhhkj jlkhl',
+  country: 'UK',
+  city: 'London',
+  website: 'http://hkjhk.com',
+};
 
 export default singleSitSpot;
--- client/src/pages/singlePlace/index.js
+++ client/src/pages/singlePlace/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/default-props-match-prop-types */
+/* eslint-disable react/require-default-props */
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import propTypes from 'prop-types';
@@ -12,107 +14,7 @@ import './style.css';
 
 class SinglePlace extends Component {
   state = {
-    reviews: [
-    {
-      id: 1,
-    reviewee: { Name: 'Hanaa', Country: 'UK', Age: '49-51' },
-    reviewDate: new Date().toLocaleDateString(),
-    title: 'OK THAT WAS Coool as  sa asfas dasdsad',
-    magicalFactors: [
-      {
-        value: { text: 'Solo Traveller Friendly', imgSrc: 'family' },
-        reviewText:
-          'kasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkj',
-      },
-      {
-        value: { text: 'Wildlife', imgSrc: 'wild' },
-        reviewText:
-          'kasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkj',
-      },
-    ],
-    tripDetails: {
-      Type: 'accomdation',
-      'Sub Type': 'private ensuite',
-      'Type of trip': 'Backpacking',
-      Date: '13/9/2018',
-      'Price Range': 'luxury',
-    },
-    reviewText:
-      'ahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfk',
-    preservation: [
-      {
-        title: 'hi',
-        badges: ['asdjlakfsjlksaj', 'sadsadasd'],
-      },
-    ],
-  },
-  {
-    id: 2,
-    reviewee: { Name: 'Hanaa', Country: 'UK', Age: '49-51' },
-    reviewDate: new Date().toLocaleDateString(),
-    title: 'OK THAT WAS Coool as  sa asfas dasdsad',
-    magicalFactors: [
-      {
-        value: { text: 'Solo Traveller Friendly', imgSrc: 'family' },
-        reviewText:
-          'kasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkj',
-      },
-      {
-        value: { text: 'Wildlife', imgSrc: 'wild' },
-        reviewText:
-          'kasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkj',
-      },
-    ],
-    tripDetails: {
-      Type: 'accomdation',
-      'Sub Type': 'private ensuite',
-      'Type of trip': 'Backpacking',
-      Date: '13/9/2018',
-      'Price Range': 'luxury',
-    },
-    reviewText:
-      'ahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfk',
-    preservation: [
-      {
-        title: 'hi',
-        badges: ['asdjlakfsjlksaj', 'sadsadasd'],
-      },
-    ],
-  },
-  {
-    id: 3,
-    reviewee: { Name: 'Hanaa', Country: 'UK', Age: '49-51' },
-    reviewDate: new Date().toLocaleDateString(),
-    title: 'OK THAT WAS Coool as  sa asfas dasdsad',
-    magicalFactors: [
-      {
-        value: { text: 'Solo Traveller Friendly', imgSrc: 'family' },
-        reviewText:
-          'kasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkj',
-      },
-      {
-        value: { text: 'Wildlife', imgSrc: 'wild' },
-        reviewText:
-          'kasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkjkasjlksajdlkj',
-      },
-    ],
-    tripDetails: {
-      Type: 'accomdation',
-      'Sub Type': 'private ensuite',
-      'Type of trip': 'Backpacking',
-      Date: '13/9/2018',
-      'Price Range': 'luxury',
-    },
-    reviewText:
-      'ahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfkahsdhaksjfhkdsjghlkhdalkhdfk',
-    preservation: [
-      {
-        title: 'hi',
-        badges: ['asdjlakfsjlksaj', 'sadsadasd'],
-      },
-    ],
-  },
-    ],
+    reviews: [],
   };
 
   render() {
@@ -123,33 +25,45 @@ class SinglePlace extends Component {
         <img
           src={backgroundImage}
           alt="background"
-          className="singlePlace-header-img"
+          className="single-place__header-img"
         />
         <div className="buttons-container">
-          <Link to= "/search"><span className="buttons-container__back">
-            <Icon type="arrow-left" />
-            <span className="buttons-container__back__text">
-              Back to results
+          <Link to="/search">
+            <span className="buttons-container__back">
+              <Icon type="arrow-left" />
+              <span className="buttons-container__back__text">
+                Back to results
+              </span>
             </span>
-          </span></Link>
-          <Link to="/review"><Button onClick={() => {}} className="buttons-container__add">
-            <Icon type="plus" style={{ color: '#fff' }} />
-            <span className="buttons-container__add__text">
-              Add your recommendation
-            </span>
-          </Button></Link>
+          </Link>
+          <Link to="/review">
+            <Button onClick={() => {}} className="buttons-container__add">
+              <Icon type="plus" style={{ color: '#fff' }} />
+              <span className="buttons-container__add__text">
+                Add your recommendation
+              </span>
+            </Button>
+          </Link>
         </div>
-        <p className="heading-name-location">{`${name}, ${country}, ${city}`} </p>
-        <a href={website} className="heading-website">{website}</a>
-        <div className="singlePlace-slider">
-        <ImageCarousel slides={images} smallTitle />
+        <p className="heading-name-location">
+          {`${name}, ${country}, ${city}`}{' '}
+        </p>
+        <a href={website} className="heading-website">
+          {website}
+        </a>
+        <div className="single-place__slider">
+          <ImageCarousel slides={images} smallTitle />
         </div>
-        <span className="recommended-by-text">Recommended by {reviews.length} contributors</span>
-        <Link to="/review"><Fab /></Link>
+        <span className="recommended-by-text">
+          Recommended by {reviews.length} contributors
+        </span>
+        <Link to="/review">
+          <Fab />
+        </Link>
         <div className="reviews-container">
-        {reviews.map(({id, reviewee, reviewDate, title, magicalFactors, tripDetails, reviewText, preservation})=>
-        <Review key={id} reviewee={reviewee} reviewDate={reviewDate} title={title} magicalFactors={magicalFactors} tripDetails={tripDetails} reviewText={reviewText} preservation={preservation} />
-    )}
+          {reviews.map(reviewItem => (
+            <Review review={reviewItem} />
+          ))}
         </div>
       </>
     );
@@ -164,12 +78,12 @@ SinglePlace.propTypes = {
   images: propTypes.arrayOf(String).isRequired,
 };
 
-SinglePlace.defaultProps ={
-  name: singleSitSpot.name, 
-  country: singleSitSpot.country, 
-  city: singleSitSpot.city, 
-  website: singleSitSpot.website, 
+SinglePlace.defaultProps = {
+  name: singleSitSpot.name,
+  country: singleSitSpot.country,
+  city: singleSitSpot.city,
+  website: singleSitSpot.website,
   images: fakeImages,
-}
+};
 
 export default SinglePlace;
