cbf8057fab3f12536a167f015a89eb927cec9e33
mossa-Sammer <mossa708496@gmail.com>
2019-11-12 18:37:03 +0200

    rename the component relates #34

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,7 +2,7 @@ import AddReview from './AddReview';
 import FAQ from './FAQ';
 import Search from './Search';
 import AboutUs from './AboutUs';
-import Singleplace from './singlePlace';
+import Singleplace from './SinglePlace';
 import Home from './Home';
 
 export { AddReview, FAQ, Search, AboutUs, Singleplace, Home };
