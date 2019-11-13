619afadd39738e04f4c90d1b926190e71255db76
mossa-Sammer <mossa708496@gmail.com>
2019-11-13 18:59:34 +0200

    refactor incoming images to add image data

--- server/controllers/routes/placeAndReviews.js
+++ server/controllers/routes/placeAndReviews.js
@@ -15,7 +15,27 @@ module.exports = (req, res, next) => {
         const error = new Error("place doesn't exists");
         error.statusCode = 404;
         throw error;
-      } else placeData = { ...places[0].fields, reviews: [] };
+      } else {
+        const place = { ...places[0].fields };
+        const images = [
+          {
+            id: place.image1,
+            src: place.image1,
+            alt: 'sitspot',
+            title: place.name,
+          },
+          {
+            id: place.image2,
+            src: place.image2,
+            alt: 'sitspot',
+            title: place.name,
+          },
+        ];
+        delete place.image1;
+        delete place.image2;
+        place.images = images;
+        placeData = { ...place, reviews: [] };
+      }
     })
     .then(() => getPlaceReviews(type, id))
     .then(incomingReviews => {
