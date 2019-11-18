8edc0ca72b4cab5d3dee44d1c50ffc7ada797102
amoodaa <amoodaa@gmail.com>
2019-11-18 03:11:54 +0200

    button fixes

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -17,12 +17,13 @@ const Button = ({ onClick, className, children, style }) => (
 Button.defaultProps = {
   children: [],
   style: {},
+  onClick: () => {},
 };
 
 Button.propTypes = {
-  onClick: propTypes.func.isRequired,
+  onClick: propTypes.func,
   className: propTypes.string.isRequired,
-  style: propTypes.shape(propTypes.any),
+  style: propTypes.shape({}),
   // children propTypes from https://stackoverflow.com/a/42122662
   children: propTypes.oneOfType([
     propTypes.arrayOf(propTypes.node),
